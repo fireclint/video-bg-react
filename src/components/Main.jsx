048b990d651b9b49a58450dda13c64d330b738fe
Clint Briley <firebriley@Clints-MBP.fios-router.home>
2022-03-14 19:20:44 -0500

    Video bg in React

---
+++ src/components/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import videoBg from '../assets/videoBg.mp4'
+
+const Main = () => {
+  return (
+    <div className='main'>
+        <div className="overlay"></div>
+        <video src={videoBg} autoPlay loop muted />
+        <div className="content">
+            <h1>Welcome</h1>
+            <p>To my site.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Main
